e5280fe4ce4b641fc89545eb25d757e7bf42737e
spatel1009 <spatel1009@gmail.com>
2018-12-11 13:48:16 -0500

    integrated new and create, started show

--- RubyOnRailsPortfolio/app/controllers/portfolios_controller.rb
+++ RubyOnRailsPortfolio/app/controllers/portfolios_controller.rb
@@ -2,4 +2,28 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.create(portfolio_params)
+      if @portfolio_item.save
+        redirect_to @portfolio_item, notice: 'Your portfolio item was created!'
+      else
+        render 'new'
+      end
+  end
+
+  def show
+  end
+
+
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body, :thumb_image, :main_image)
+  end
 end
