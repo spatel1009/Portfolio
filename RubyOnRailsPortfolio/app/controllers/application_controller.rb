d68da23b13c733c0641e6a1486ecccac5a9d44ab
spatel1009 <spatel1009@gmail.com>
2018-12-12 14:50:53 -0500

    added guest user for current user

--- RubyOnRailsPortfolio/app/controllers/application_controller.rb
+++ RubyOnRailsPortfolio/app/controllers/application_controller.rb
@@ -2,8 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include SetSource
-
-  def current_user
-    super || 
-  end
+  include CurrentUserConcern
 end
