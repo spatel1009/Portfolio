7dabcedd37af1f8ef85a3255ea15b5c4ec0598ba
spatel1009 <spatel1009@gmail.com>
2018-12-12 14:31:09 -0500

    added ostruct and pry

--- RubyOnRailsPortfolio/app/controllers/application_controller.rb
+++ RubyOnRailsPortfolio/app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   include DeviseWhitelist
   include SetSource
+
+  def current_user
+    super || 
+  end
 end
