c32d13ac9fef596f2459af281aa1378686f007a9
spatel1009 <spatel1009@gmail.com>
2018-12-14 23:39:16 -0500

    implemented show page styles for portfolio layout

--- RubyOnRailsPortfolio/db/seeds.rb
+++ RubyOnRailsPortfolio/db/seeds.rb
@@ -24,7 +24,7 @@ end
     title: "Portfolio title: #{portfolio_item}",
     subtitle: "Ruby on Rails",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "https://via.placeholder.com/700x400",
+    main_image: "https://via.placeholder.com/600x400",
     thumb_image:"https://via.placeholder.com/350x200"
   )
 end
@@ -34,7 +34,7 @@ end
     title: "Portfolio title: #{portfolio_item}",
     subtitle: "Java",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "https://via.placeholder.com/700x400",
+    main_image: "https://via.placeholder.com/600x400",
     thumb_image:"https://via.placeholder.com/350x200"
   )
 end
