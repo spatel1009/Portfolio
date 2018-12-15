5a271d651dcae398544ef700b5dae3a98fbb8df2
spatel1009 <spatel1009@gmail.com>
2018-12-15 15:10:01 -0500

    added position to portfolio for ordering

--- RubyOnRailsPortfolio/db/schema.rb
+++ RubyOnRailsPortfolio/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_13_203137) do
+ActiveRecord::Schema.define(version: 2018_12_15_200409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2018_12_13_203137) do
     t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
   end
 
   create_table "skills", force: :cascade do |t|
