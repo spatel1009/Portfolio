fc3459f7ad172317780b9124706c56cdd0fcb6c9
spatel1009 <spatel1009@gmail.com>
2018-12-15 13:48:23 -0500

    implemented refactor for nav and active statuses

--- RubyOnRailsPortfolio/app/helpers/application_helper.rb
+++ RubyOnRailsPortfolio/app/helpers/application_helper.rb
@@ -19,4 +19,38 @@ module ApplicationHelper
   def copyright_generator
     SohelViewTool::Renderer.copyright 'Sohel Patel', 'All rights reserved.'
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_path,
+        title: 'About Me'
+      },
+      {
+        url: resume_path,
+        title: 'Resume'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+    ]
+  end
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    'active' if current_page? path
+  end
 end
