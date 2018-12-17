d9399256f40e435f0cc18e648c76d70c8eb22220
spatel1009 <spatel1009@gmail.com>
2018-12-17 16:45:25 -0500

    implemented video background

--- RubyOnRailsPortfolio/config/initializers/assets.rb
+++ RubyOnRailsPortfolio/config/initializers/assets.rb
@@ -10,6 +10,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( blogs.scss )
 Rails.application.config.assets.precompile += %w( portfolios.scss )
 Rails.application.config.assets.precompile += %w( orbit-1.scss )
+Rails.application.config.assets.paths << "#{Rails.root}/app/assets/videos" 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
