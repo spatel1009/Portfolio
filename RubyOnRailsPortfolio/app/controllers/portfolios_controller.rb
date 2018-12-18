77bac7538675f5f8004711065bb89ad69ac4d7bd
spatel1009 <spatel1009@gmail.com>
2018-12-18 16:52:04 -0500

    added edit button, added ability to add new technologies using jquery

--- RubyOnRailsPortfolio/app/controllers/portfolios_controller.rb
+++ RubyOnRailsPortfolio/app/controllers/portfolios_controller.rb
@@ -17,7 +17,6 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
-    3.times { @portfolio_item.technologies.build }
   end
 
   def java
@@ -37,7 +36,6 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    3.times { @portfolio_item.technologies.build }
   end
 
   def update
@@ -68,6 +66,6 @@ class PortfoliosController < ApplicationController
                                       :body, 
                                       :thumb_image, 
                                       :main_image, 
-                                      technologies_attributes: [:name])
+                                      technologies_attributes: [:id, :name, :_destroy])
   end
 end
