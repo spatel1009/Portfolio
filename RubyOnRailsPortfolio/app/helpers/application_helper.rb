4eb9729bd6806e0a3708f5d3a1313a2071044599
spatel1009 <spatel1009@gmail.com>
2018-12-18 18:27:49 -0500

    implemented gritter for notifications

--- RubyOnRailsPortfolio/app/helpers/application_helper.rb
+++ RubyOnRailsPortfolio/app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
   def login_helper style = ''
-    if current_user.is_a?(GuestUser)
-      (link_to 'Register', new_user_registration_path, class: style) + 
-      " ".html_safe +
-      (link_to 'Login', new_user_session_path, class: style) 
-    else 
+    if logged_in?(:site_admin)
       link_to 'Logout', destroy_user_session_path, method: :delete, class: style 
     end
   end
@@ -49,6 +45,7 @@ module ApplicationHelper
 
     nav_links.html_safe
   end
+
 
   def active? path
     'active' if current_page? path
