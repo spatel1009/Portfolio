6889a75a0709bf30cd166a76190c75d6c5ab2161
spatel1009 <spatel1009@gmail.com>
2018-12-18 21:44:30 -0500

    built out initial form and partial for comments

--- RubyOnRailsPortfolio/app/controllers/blogs_controller.rb
+++ RubyOnRailsPortfolio/app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_blog, only: [:edit, :update, :destroy, :toggle_status]
   layout 'blog'
   access user:{except: [:show, :index, :destroy, :new, :create, :edit, :update, :toggle_status]}, site_admin: :all
   # GET /blogs
@@ -12,6 +12,8 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
+    @blog = Blog.includes(:comments).friendly.find(params[:id])
+    @comment = Comment.new
     @page_title = @blog.title
   end
 
