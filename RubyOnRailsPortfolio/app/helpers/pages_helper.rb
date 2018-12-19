e2c03b39e7c63ed1b042d143776b0317362420f0
spatel1009 <spatel1009@gmail.com>
2018-12-18 20:48:04 -0500

    created parser to make links clickable on tweets

--- RubyOnRailsPortfolio/app/helpers/pages_helper.rb
+++ RubyOnRailsPortfolio/app/helpers/pages_helper.rb
@@ -6,4 +6,28 @@ module PagesHelper
   def myresume
     link_to '(My Resume)', resume_path
   end
+
+  def twitter_parser tweet
+    regex = %r{
+      \b
+      (
+        (?: [a-z][\w-]+:
+         (?: /{1,3} | [a-z0-9%] ) |
+          www\d{0,3}[.] |
+          [a-z0-9.\-]+[.][a-z]{2,4}/
+        )
+        (?:
+         [^\s()<>]+ | \(([^\s()<>]+|(\([^\s()<>]+\)))*\)
+        )+
+        (?:
+          \(([^\s()<>]+|(\([^\s()<>]+\)))*\) |
+          [^\s`!()\[\]{};:'".,<>?«»“”‘’]
+        )
+      )
+    }ix
+
+    tweet.gsub(regex) do |url|
+      "<a href='#{url}' target = '_blank'>#{url}</a>"
+    end.html_safe
+  end
 end
